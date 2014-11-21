15e2b86f239fff756c37bc51ed1e361527f265eb
TJ VanToll <tj.vantoll@gmail.com>
2014-11-21 10:46:45 -0500

    Site updated at 2014-11-21 15:46:45 UTC

---
+++ speaking/slides/Telerik-Platform/Columbus/js/config.js
@@ -0,0 +1,193 @@
+var presentationConfig = {
+	"dependencies": [
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		}
+	]
+};
