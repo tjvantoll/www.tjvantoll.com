00b9920e0c7ad0176ea3e1ab3408aafaa41ef32e
TJ VanToll <tj.vantoll@gmail.com>
2019-08-30 11:07:36 -0400

    Disabling Google analytics

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,3 +1,4 @@
+/*
 // Google Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -6,6 +7,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-29179796-1', 'auto');
 ga('send', 'pageview');
+*/
 
 // Ads
 (function() {
